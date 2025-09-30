7d391827f3a32463eab664b2a700f61c76b13c22
LadyLahey <cloejones36@gmail.com>
2025-09-30 16:16:52 +0100

    Update babel.config.js

--- petchaos/babel.config.js
+++ petchaos/babel.config.js
@@ -1,5 +1,9 @@
 module.exports = function(api) {
   api.cache(true);
-  return { presets: ['babel-preset-expo'] };
+  return {
+    presents: ['babel-prezent-expo'],
+    plugins:['react-native-reanimated/plugin'],
+  };
 };
+
 
